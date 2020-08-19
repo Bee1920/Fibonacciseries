66a902f1915da322fc312c381a3e2e1a86939b24
Bee1920 <69208814+Bee1920@users.noreply.github.com>
2020-08-19 20:33:56 +0530

    Update postivieno's.py

--- postivieno's.py
+++ postivieno's.py
@@ -1,2 +1,9 @@
- input_string=input("Enter "
- 
+ list = []
+ n = int(input("Enter the number of elements:"))
+ for i in range(0, n):
+  element = int(input())
+  list.append(element)
+ print("Positive numbers in list:")
+ for j in range(n):
+  if(list[j] >= 0):
+   print(list[j], end = ' ')
