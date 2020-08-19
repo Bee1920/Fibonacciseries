59dea96a458d69365fd04e8604c0f990a7f619ce
Bee1920 <69208814+Bee1920@users.noreply.github.com>
2020-08-20 00:12:54 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,3 +1,4 @@
+#assigning elements to differnet lists
 l = [4, 5]
 l.append(6)
 print(l)
@@ -5,3 +6,12 @@ l.insert(2,19)
 print(l)
 l.extend(range(12,18))
 print(l)
+
+#accessing elements from a tuple
+tu = ("Happy", "Birth", "Day", "to", "you")
+print(tu[1:3])
+      
+#deleting differnt dictonary elements
+dictonary = { 1: 'Happy', 2: 'Birth', 3:'Day', 4: 'to', 5: 'you'}
+print(dictionary.pop(3))
+print(dictionary.pop(4))
